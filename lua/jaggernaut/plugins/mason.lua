b87bcf136bb7d4dfe1e52c1e2cb06dfc04260785
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-03-25 07:06:33 +0000

    Add support for automatic installation of lsp servers

--- lua/jaggernaut/plugins/mason.lua
+++ lua/jaggernaut/plugins/mason.lua
@@ -2,9 +2,16 @@ return {
 	"williamboman/mason.nvim",
 	dependencies = {
 		"jay-babu/mason-nvim-dap.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
 	},
 	config = function()
 		require("mason").setup()
-    require("mason-nvim-dap").setup()
+		require("mason-nvim-dap").setup({
+			ensure_installed = { "cppdbg", "python" },
+		})
+		require("mason-lspconfig").setup({
+			ensure_installed = { "lua_ls", "rust_analyzer", "clangd" },
+		})
 	end,
 }
