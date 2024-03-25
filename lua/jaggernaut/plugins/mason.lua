0dd52aa149e418da6ca1564ce4de0f2fb039246d
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-03-25 05:40:05 +0000

    Add support for DAP

--- lua/jaggernaut/plugins/mason.lua
+++ lua/jaggernaut/plugins/mason.lua
@@ -1,6 +1,10 @@
 return {
 	"williamboman/mason.nvim",
+	dependencies = {
+		"jay-babu/mason-nvim-dap.nvim",
+	},
 	config = function()
 		require("mason").setup()
+    require("mason-nvim-dap").setup()
 	end,
 }
--- lua/jaggernaut/plugins/nvim-dap.lua
+++ lua/jaggernaut/plugins/nvim-dap.lua
@@ -1,4 +1,35 @@
 return {
-  "mfussenegger/nvim-dap",
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"theHamsta/nvim-dap-virtual-text",
+		"Weissle/persistent-breakpoints.nvim",
+		"williamboman/mason.nvim",
+	},
+	config = function()
+		local dap = require("dap")
+		local mason_nvim_dap = require("mason-nvim-dap")
+		-- Keybindings
+		vim.keymap.set("n", "<F1>", ":lua require'dap'.repl.open()<CR>")
+		vim.keymap.set("n", "<F11>", ":lua require'dap'.continue()<CR>")
+		vim.keymap.set("n", "<F12>", "lua require'dap'.terminate()<CR>")
+		vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
+		vim.keymap.set("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+		vim.keymap.set(
+			"n",
+			"<leader>lp",
+			":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>"
+		)
+		-- Setup dap
+		mason_nvim_dap.setup({
+			ensure_installed = { "cppdbg", "python" },
+		})
 
+    dap.adapters.cppdbg = {
+      id = 'cppdbg',
+      type = 'executable',
+      command = '/root/.local/share/nvim/mason/bin/OpenDebugAD7',
+    }
+
+		require("dap.ext.vscode").load_launchjs(nil, { cppdbg = { "c", "cpp", "rust" } })
+	end,
 }
