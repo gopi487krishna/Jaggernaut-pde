490f0fdcdfe1d2ced796ece81e171cb9c5018f10
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-03-25 12:36:17 +0000

    Modify keybindings for telescope

--- lua/jaggernaut/plugins/telescope.lua
+++ lua/jaggernaut/plugins/telescope.lua
@@ -6,10 +6,10 @@ return {
     require("telescope").setup({})
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
-    vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+    vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>th', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>tm', builtin.man_pages, {})
+    vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, {})
   end
 }
