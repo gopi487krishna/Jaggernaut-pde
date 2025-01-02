3831c21746c06a8bc1f982594d22713d7ad52bfb
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-01-02 08:32:07 +0530

    Add time to lualine

--- lua/jaggernaut/plugins/lualine.lua
+++ lua/jaggernaut/plugins/lualine.lua
@@ -1,7 +1,15 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function ()
-    require('lualine').setup()
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_y = {
+					function()
+						return os.date("%H:%M:%S")
+					end,
+				},
+			},
+		})
+	end,
 }
